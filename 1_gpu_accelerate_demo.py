1f196d541a2670147059d06981c1f9a7aa70f205
GuiQing <903078494@qq.com>
2021-09-02 01:19:09 +0800

    add 1_gpu_accelerate_demo

---
+++ 1_gpu_accelerate_demo.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+"""
+	PyTorch的GPU加速
+	@author: GuiQing
+	@time: 2021-09-02 0:29	
+"""
+
+import torch 
+import time
+
+print(torch.__version__)  
+print(torch.cuda.is_available())
+
+a = torch.randn(10000, 1000)
+b = torch.randn(1000, 2000)
+
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
+
+device = torch.device('cuda')
+a = a.to(device)
+b = b.to(device)
+
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
+
+t0 = time.time()
+c = torch.matmul(a, b)
+t1 = time.time()
+print(a.device, t1 - t0, c.norm(2))
